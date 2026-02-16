f9e14ab1d8cc6baf6e000bb367cc8686d9e774fe
Ronan Martins <ronanmartinsoliveira10@gmail.com>
2026-02-16 10:36:54 -0300

    att

--- backend/src/services/user/DetailUserService.ts
+++ backend/src/services/user/DetailUserService.ts
@@ -1,26 +1,31 @@
 import prismaClient from "../../prisma/index";
 
 class DetailUserService {
-    async execute(user_id: string) {
-        const user = await prismaClient.user.findFirst({
-            where: {
-                id: user_id,
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                role: true,
-                createdAt: true,
-            },
-        });
+  async execute(user_id: string) {
+    try {
+      const user = await prismaClient.user.findFirst({
+        where: {
+          id: user_id,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          createdAt: true,
+        },
+      });
 
-        if (!user) {
-            throw new Error("Usuario não encontrado")
-        }
+      if (!user) {
+        throw new Error("Usuario não encontrado");
+      }
 
-        return user;
+      return user;
+    } catch (err) {
+        console.log(err);
+        throw new Error("Usuario não encontrado");
     }
+  }
 }
 
 export { DetailUserService };
